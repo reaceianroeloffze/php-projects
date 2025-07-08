cad78238a573996a2f30a8209f4d70a04e2ad8b2
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-08 14:52:29 +0200

    Add Culinary Cove Restaurant Site to repo

---
+++ culinary-cove-restaurant/inc/header.inc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/simple.css" />
+    <link rel="stylesheet" href="../styles/custom.css" />
+    <title>Culinary Cove &bull; <?php echo $pageTitle; ?></title>
+</head>
+<body>
+  <header class="header-with-background" style="background-image: url('<?php echo $headerImg; ?>'); ">
+    <h1>Culinary Cove</h1>
+    <p>Your sanctuary for exceptional flavours</p>
+    <nav>
+      <a href="/culinary-cove-restaurant/">Our mission</a>
+      <a href="/culinary-cove-restaurant/ingredients.php">Ingredients</a>
+      <a href="/culinary-cove-restaurant/menu.php">Menu</a>
+    </nav>
+  </header>
+
+  <main>

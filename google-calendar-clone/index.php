408843f4eb75f78c363d56de22ae3e645210036d
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-09 16:42:51 +0200

    Add style.css & script.js files

--- google-calendar-clone/index.php
+++ google-calendar-clone/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" dir="ltr" xmlns:link="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -7,10 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="Reace Ian Roeloffze">
     <meta name="description" content="Google Calendar Clone Project">
-    <title>Document</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Google Calendar Clone</title>
+    <script src="/js/script.js" defer></script>
 </head>
 <body>
+<header class="header">
+    <h1 class="page-title">📅 Course Calendar <br> Google Calendar Clone Project</h1>
 
+    <!-- Clock -->
+    <div class="clock-container">
+        <div class="clock-container__clock"></div>
+    </div>
+</header>
 </body>
 </html>
-<?php
+

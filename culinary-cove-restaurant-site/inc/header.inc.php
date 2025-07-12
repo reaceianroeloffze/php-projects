928a6830cb40490bf6e1730033b1d3b6d72b1212
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-12 09:00:14 +0200

    Modify CSS path

--- culinary-cove-restaurant-site/inc/header.inc.php
+++ culinary-cove-restaurant-site/inc/header.inc.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/simple.css"/>
-    <link rel="stylesheet" href="../styles/custom.css"/>
+    <link rel="stylesheet" href="./styles/simple.css"/>
+    <link rel="stylesheet" href="./styles/custom.css"/>
     <!-- Dynamically add the page currently on to title. -->
     <title>Culinary Cove &bull; <?php echo !empty($pageTitle) ? $pageTitle : $pageTitle = 'Home'; ?></title>
 </head>

17776d77aa76b6004d42aaec23707dcd427e7d39
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-14 14:43:50 +0200

    Retrieve necessary DOM elements

--- google-calendar-clone/js/script.js
+++ google-calendar-clone/js/script.js
@@ -1,1 +1,10 @@
+/** ===================================================
+ * Frontend logic for the Google Calendar Clone Project
+ * ==================================================== */
+
 'use strict';
+
+// Retrieve all necessary document elements
+const calendarEl = document.querySelector('.calendar');
+const monthYearEl = document.querySelector('.month-and-year');
+const modalEl = document.querySelector('.modal');

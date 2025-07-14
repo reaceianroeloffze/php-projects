a68b6abd0cc0277375e5dea11e35cbd0dedd6e82
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-14 09:44:26 +0200

    Add createAppointment() function

--- google-calendar-clone/model/appointments-model.php
+++ google-calendar-clone/model/appointments-model.php
@@ -1,2 +1,19 @@
 <?php
 
+// Create a function to create a new appointment
+function createAppointment($courseName, $instructorName, $startDate, $endDate): int
+{
+    $db = connectToMySQL();
+    $sql = 'INSERT INTO appointments (course_name, instructor_name, start_date, end_date) VALUES (:course_name, :instructor_name, :start_date, :end_date)';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $stmt->bindValue(':course_name', $courseName, PDO::PARAM_STR);
+    $stmt->bindValue(':instructor_name', $instructorName, PDO::PARAM_STR);
+    $stmt->bindValue(':start_date', $startDate, PDO::PARAM_STR);
+    $stmt->bindValue(':end_date', $endDate, PDO::PARAM_STR);
+    $stmt->execute();
+    $updatedRows = $stmt->rowCount();
+    $stmt->closeCursor();
+    return $updatedRows;
+}
+

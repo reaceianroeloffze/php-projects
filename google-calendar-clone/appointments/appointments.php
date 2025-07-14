7bd1cb8134f42817ca6a98181a6ab748d1234836
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-14 09:45:46 +0200

    Add require_once functions and action posting or getting

--- google-calendar-clone/appointments/appointments.php
+++ google-calendar-clone/appointments/appointments.php
@@ -4,4 +4,13 @@
  * ================================== */
 
 // include connection.php file to connect to the database
-include '../library/connections.php';
+require_once '../library/connections.php';
+// Require the appointments' model file.
+require_once '../model/appointments-model.php';
+
+$dbEvents = []; // Store events from the database
+
+// Post or get input with a name "action"
+$action = trim(filter_input(INPUT_POST, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS))
+    ?? $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+

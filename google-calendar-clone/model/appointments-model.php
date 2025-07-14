345c7ca5c3534eb7cced0ad8a7e47c06d6ed5519
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-14 10:46:22 +0200

    add updateAppointment function to the model

--- google-calendar-clone/model/appointments-model.php
+++ google-calendar-clone/model/appointments-model.php
@@ -12,8 +12,25 @@ function createAppointment($courseName, $instructorName, $startDate, $endDate): 
     $stmt->bindValue(':start_date', $startDate, PDO::PARAM_STR);
     $stmt->bindValue(':end_date', $endDate, PDO::PARAM_STR);
     $stmt->execute();
-    $updatedRows = $stmt->rowCount();
+    $rowsAdded = $stmt->rowCount();
     $stmt->closeCursor();
-    return $updatedRows;
+    return $rowsAdded;
+}
+
+// Create a function to update an existing appointment
+function updateAppointment($courseId, $courseName, $instructorName, $startDate, $endDate): int {
+    $db = connectToMySQL();
+    $sql = 'UPDATE appointments SET course_name = :course_name, instructor_name = :instructor_name, start_date = :start_date, end_date = :end_date WHERE course_id = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $stmt->bindValue(':course_name', $courseName, PDO::PARAM_STR);
+    $stmt->bindValue(':instructor_name', $instructorName, PDO::PARAM_STR);
+    $stmt->bindValue(':start_date', $startDate, PDO::PARAM_STR);
+    $stmt->bindValue(':end_date', $endDate, PDO::PARAM_STR);
+    $stmt->bindValue(':id', $courseId, PDO::PARAM_INT);
+    $stmt->execute();
+    $rowsUpdated = $stmt->rowCount();
+    $stmt->closeCursor();
+    return $rowsUpdated;
 }
 

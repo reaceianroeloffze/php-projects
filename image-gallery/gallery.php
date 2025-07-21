ee36bfc60ca511af3daaed11c1c131b34b45f187
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-21 18:38:08 +0200

    Style images and container

--- image-gallery/gallery.php
+++ image-gallery/gallery.php
@@ -2,15 +2,22 @@
 require_once './inc/functions.inc.php';
 require_once './inc/images.inc.php';
 
+$increment = 0;
+
 ?>
 <?php require_once './views/header.php'; ?>
 <div class="img-container">
-    <figure class="figure">
-        <?php foreach($imageTitles AS $filename => $title) : ?>
-        <img src="images/<?php echo $filename; ?>" alt="<?php echo $title; ?>" class="figure-img img-fluid rounded">
-        <figcaption class="figure-caption"><?php echo $title; ?></figcaption>
-        <?php endforeach; ?>
-    </figure>
+    <?php foreach ($imageTitles as $filename => $title) : ?>
+        <figure class="figure image">
+            <a href="./image.php">
+                <img src="images/<?php echo rawurldecode($filename); ?>" alt="<?php echo e($title); ?>"
+                     class="figure-img <?php echo 'img' . ++$increment; ?> ">
+            </a>
+            <figcaption class="image-caption"><a
+                        href=" ./image.php?image=<?= rawurldecode($filename) ?>"><?php echo e($title); ?></a>
+            </figcaption>
+        </figure>
+    <?php endforeach; ?>
 </div>
 
 <?php include './views/footer.php'; ?>
--- image-gallery/image.php
+++ image-gallery/image.php
@@ -1,9 +1,13 @@
 <?php
-include './inc/functions.inc.php';
-include './inc/images.inc.php';
+require_once './inc/functions.inc.php';
+require_once './inc/images.inc.php';
 
 ?>
 <?php include './views/header.php'; ?>
 
+<?php
+
+
+?>
 
 <?php include './views/footer.php'; ?>

814ccfc8aee1035647071cb6c74bc3e576e8ca87
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-22 09:40:38 +0200

    Refactor & refine logic and styles

--- image-gallery/image.php
+++ image-gallery/image.php
@@ -7,16 +7,31 @@ require_once './inc/images.inc.php';
 
 <?php
 
-if (!empty($_GET['image'])) {
-    $image = $_GET['image'];
+// Check if the image key is not empty and exists in the given associative array
+/*if (!empty($_GET['image']) && array_key_exists($_GET['image'], $imageTitles)) {
+    $image = rawurldecode($_GET['image']);
     $title = $imageTitles[$image];
     $description = $imageDescriptions[$image];
     echo "<h1 class='img-title'>$title</h1>";
     echo "<img src='./images/$image' alt='$title'>";
     echo "<p>$description</p>";
-}
+}*/
 
 ?>
+
+<!-- Alternative syntax -->
+<?php if (!empty($_GET['image']) && array_key_exists($_GET['image'], $imageTitles)): ?>
+    <?php
+        $image = rawurldecode($_GET['image']);
+        $title = $imageTitles[$image];
+        $description = $imageDescriptions[$image];
+    ?>
+    <h1 class="img-title"><?php echo e($title); ?></h1>
+    <img src="./images/<?php echo e($image); ?>" alt="<?php echo e($title); ?>">
+    <p><?php echo e($description); ?></p>
+<?php else: ?>
+    <h1>Image not found</h1>
+<?php endif; ?>
 
 <a href="gallery.php">Back to Gallery</a>
 

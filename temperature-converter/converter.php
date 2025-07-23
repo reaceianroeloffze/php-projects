2f1871312a35d3c849be3df48d0de4e723b84495
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-23 14:01:41 +0200

    debug and refactor logic, handle exceptions

--- temperature-converter/converter.php
+++ temperature-converter/converter.php
@@ -28,47 +28,58 @@ $units = [
 $temperature = 0;
 $unit_from = '';
 $unit_to = '';
-$converted_temperature = 0;
+$converted_temperature = '';
 $error = '';
 
-// If all the submitted data is obtained, store them in the initialised variables
-if (isset($_GET['temp']) && !empty($_GET['unit_from']) && !empty($_GET['unit_to'])) {
-    $temperature = e($_GET['temp']);
-    $unit_from = e($_GET['unit_from']);
-    $unit_to = e($_GET['unit_to']);
-}
+// Validate input
+if (isset($_GET['temp'], $_GET['unit_from'], $_GET['unit_to'])) {
 
-// Convert the numerical value based on the from and to units submitted
-switch ($unit_from . '_' . $unit_to) {
+    // Trim input to avoid accidental whitespace
+    $temp_input = trim($_GET['temp']);
 
-    // Celsius to Kelvin
-    case 'celsius' . '_' . 'kelvin' :
-        $converted_temperature = convertToKelvinFromCelsius($temperature) . ' ' . $kelvinUnit;
-        break;
+    // Display error messages for invalid input
+    if ($temp_input === '' || !is_numeric($temp_input)) {
+        $error = 'Please enter a number to convert.';
+    } else if ($_GET['unit_from'] === '' || $_GET['unit_to'] === '') {
+        $error = 'Select both units to convert the temperature.';
+    } else if ($_GET['unit_from'] === $_GET['unit_to']) {
+        $error = "Can't convert from and to the same unit.";
+    } else {
+        // Process submitted data if no errors
+        $temperature = (float)e($temp_input);
+        $unit_from = e($_GET['unit_from']);
+        $unit_to = e($_GET['unit_to']);
 
-    // Celsius to Fahrenheit
-    case 'celsius' . '_' . 'fahrenheit' :
-        $converted_temperature = convertToFahrenheitFromCelsius($temperature) . ' ' . $fahrenheitUnit;
-        break;
+        // Convert temperature based on from and to units
+        switch ($unit_from . '_' . $unit_to) {
+            // Celsius to Kelvin
+            case 'celsius_kelvin':
+                $converted_temperature = convertToKelvinFromCelsius($temperature) . ' ' . $kelvinUnit;
+                break;
+            // Celsius to Fahrenheit
+            case 'celsius_fahrenheit':
+                $converted_temperature = convertToFahrenheitFromCelsius($temperature) . ' ' . $fahrenheitUnit;
+                break;
+            // Fahrenheit to Kelvin
+            case 'fahrenheit_kelvin':
+                $converted_temperature = convertToKelvinFromFahrenheit($temperature) . ' ' . $kelvinUnit;
+                break;
+            // Fahrenheit to Celsius
+            case 'fahrenheit_celsius':
+                $converted_temperature = convertToCelsiusFromFahrenheit($temperature) . ' ' . $celsiusUnit;
+                break;
+            // Kelvin to Celsius
+            case 'kelvin_celsius':
+                $converted_temperature = convertToCelsiusFromKelvin($temperature) . ' ' . $celsiusUnit;
+                break;
+            // Kelvin to Fahrenheit
+            case 'kelvin_fahrenheit':
+                $converted_temperature = convertToFahrenheitFromKelvin($temperature) . ' ' . $fahrenheitUnit;
+                break;
 
-    // Fahrenheit to Kelvin
-    case 'fahrenheit' . '_' . 'kelvin' :
-        $converted_temperature = convertToKelvinFromFahrenheit($temperature) . ' ' . $kelvinUnit;
-        break;
-
-    // Fahrenheit to Celsius
-    case 'fahrenheit' . '_' . 'celsius' :
-        $converted_temperature = convertToCelsiusFromFahrenheit($temperature) . ' ' . $celsiusUnit;
-        break;
-
-    // Kelvin to Celsius
-    case 'kelvin' . '_' . 'celsius' :
-        $converted_temperature = convertToCelsiusFromKelvin($temperature) . ' ' . $celsiusUnit;
-        break;
-
-    // Kelvin to Fahrenheit
-    case 'kelvin' . '_' . 'fahrenheit' :
-        $converted_temperature = convertToFahrenheitFromKelvin($temperature) . ' ' . $fahrenheitUnit;
-        break;
-
+            default:
+                $error = 'Invalid unit conversion selected.';
+                break;
+        }
+    }
 }

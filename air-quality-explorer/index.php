05594d40738b4b2f4dd7a6f4e1f34aafc480a5cb
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-02 14:26:18 +0200

    Display Countries and Country Codes

--- air-quality-explorer/index.php
+++ air-quality-explorer/index.php
@@ -7,8 +7,11 @@ require_once __DIR__ . '/inc/api.inc.php';
 <?php require __DIR__ . '/views/header.inc.php'; ?>
 
     <ul>
-        <?php foreach ($filteredResponseArray as $country) { ?>
-            <li><?php echo "{$country['name']} ({$country['code']})"; ?></li>
+        <?php foreach (($filteredResponseArray ?? '') as $country) { ?>
+            <li>
+                <?php echo e($country['name']) ?>
+                (<?php echo e($country['code']); ?>)
+            </li>
         <?php } ?>
     </ul>
 

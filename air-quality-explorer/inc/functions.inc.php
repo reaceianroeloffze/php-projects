1a3ba5f81a0e44e6f0f2ba4b0461d1724d06d1c9
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-04 14:30:31 +0200

    Add generateHeaders(), generateGetRequest() & generateResponseBody() functions

--- air-quality-explorer/inc/functions.inc.php
+++ air-quality-explorer/inc/functions.inc.php
@@ -1,5 +1,24 @@
 <?php
 
-function e($value) {
+function e($value)
+{
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
+
+// Create a function to generate header content
+function generateHeaders(): array
+{
+    return ['headers' => [
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+        'X-API-KEY' => getenv('OpenAQ_API_KEY'),
+    ]];
+}
+
+function generateGetRequest($client, $path) {
+    return $client->get($path, generateHeaders());
+};
+
+function generateResponseBody($response): array {
+    return json_decode($response->getBody(), true);
+};

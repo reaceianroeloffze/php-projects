7718d4d877229ed84694f167414f0c024692f2c4
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-14 14:01:12 +0200

    Add filterLocationsByCountry() function

--- air-quality-explorer/inc/functions.inc.php
+++ air-quality-explorer/inc/functions.inc.php
@@ -15,10 +15,60 @@ function generateHeaders(): array
     ]];
 }
 
-function generateGetRequest($client, $path) {
+function generateGetRequest($client, $path)
+{
     return $client->get($path, generateHeaders());
-};
+}
 
-function generateResponseBody($response): array {
+;
+
+function generateResponseBody($response): array
+{
     return json_decode($response->getBody(), true);
+}
+
+/**
+ * Filter & group air quality locations by country
+ *
+ * @param array $locations All location data from the API
+ * @param array $parameters Sensor parameter names to keep (lowercase)
+ * @param int $maxLocationsPerCountry Maximum locations to display per country
+ *
+ * @return array Grouped and filtered array
+ */
+
+function filterLocationsByCountry(array $locations, array $parameters, int $maxLocationsPerCountry): array
+{
+    $locationsGroupedByCountry = [];
+
+    foreach ($locations as $location) {
+        $requiredSensors = array_values(array_filter(
+            $location['sensors'],
+            fn($sensor) => in_array(strtolower($sensor['parameter']['name']), $parameters)
+        ));
+
+        // Skip locations that do not have the required sensors
+        if (empty($requiredSensors)) {
+            continue;
+        }
+
+        $country = $location['country']['name'];
+
+        // Initialise a country array if not set
+        if (!isset($locationsGroupedByCountry[$country])) {
+            $locationsGroupedByCountry[$country] = [];
+        }
+
+        // Append country and locations based on maximum locations set
+        if (count($locationsGroupedByCountry[$country]) < $maxLocationsPerCountry) {
+            $location['sensors'] = $requiredSensors;
+            $locationsGroupedByCountry[$country][] = $location;
+        }
+    }
+
+    // Sort by country name
+    ksort($locationsGroupedByCountry);
+
+    // return the filtered array
+    return $locationsGroupedByCountry;
 }

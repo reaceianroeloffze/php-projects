0be190d499791b632e0ed8bf68ba3486f5e2e214
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-14 09:40:11 +0200

    request country data & filter it

--- air-quality-explorer/inc/api.inc.php
+++ air-quality-explorer/inc/api.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /** ============================================
  * The API handler file for Air Quality Explorer
  * ============================================= */
@@ -8,19 +7,21 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/functions.inc.php';
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Uri;
+
 // Start a new Guzzle session with base uri
-$client = new GuzzleHttp\Client(['base_uri' =>'https://api.openaq.org']);
+$client = new Client(['base_uri' => 'https://api.openaq.org']);
 
 // Retrieve API country data
-$response_countries = $client->get('/v3/countries?limit=1000', [
-    'headers' => [
-        'Content-Type' => 'application/json',
-        'X-API-KEY' => getenv('OpenAQ_API_KEY'), // Use your own api key
-    ]
-]);
-
+$response_countries = generateGetRequest($client, '/v3/countries?limit=1000');
 // Decode the response and turn it into a multidimensional array
-$responseArrayCountries = json_decode($response_countries->getBody(), true);
+$responseArrayCountries = generateResponseBody($response_countries);
+$responseArrayCountries = $responseArrayCountries['results'];
 
 // Provide an array of specific countries
 $SelectedCountries = [
@@ -34,9 +35,8 @@ $SelectedCountries = [
     'Italy',
 ];
 
-// Filter the response multidimensional to contain only the selected countries
-$filteredResponseArray = array_filter($responseArrayCountries['results'], fn ($country) => in_array($country['name'], $SelectedCountries));
-print_r($filteredResponseArray);
+// Filter the response multidimensional array to contain only the selected countries
+$filteredCountriesResponseArray = array_filter($responseArrayCountries, fn($country) => in_array($country['name'], $SelectedCountries));
 
 // Alternate syntax for filtering. Using for reference
 /*
@@ -46,30 +46,10 @@ $filteredResponseArray = array_filter($responseArray['results'], function ($coun
 */
 
 // Store the id of each country in the multidimensional array in a variable
-$country_id = '';
-foreach ($filteredResponseArray AS $country) {
-    $country_id = $country['id'];
+$country_ids = [];
+foreach ($filteredCountriesResponseArray as $country) {
+    $country_ids[$country['name']] = $country['id'];
 }
-
-// Request the locations of air quality stations based on the country's id
-$response_locations = $client->get("/v3/locations?countries_id={$country_id}&limit=1000", [
-    'headers' => [
-        'Content-Type' => 'application/json',
-        'X-API-KEY' => getenv('OpenAQ_API_KEY'),
-    ]
-]);
-
-// Convert response to a multidimensional array
-$responseArrayLocations = json_decode($response_locations->getBody(), true);
-print_r($responseArrayLocations);
-
-// Store the id of each location in a variable
-$location_id = '';
-foreach ($responseArrayLocations['results'] AS $location) {
-    $location_id = $location['id'];
-}
-
-// Get air quality sensors based on the location's id
-$response_sensors = $client->get("/v3/location/{$location_id}/sensors", []);
-$responseArraySensors = json_decode($response_sensors->getBody(), true);
-
+$country_ids = array_values($country_ids);
+$country_id_string = implode(',', $country_ids);
+// print_r($country_ids);

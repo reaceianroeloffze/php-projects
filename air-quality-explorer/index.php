7c6d4259fcd56bc92c1284198aa7a96eee32a02e
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-18 18:28:26 +0200

    Request countries and display them

--- air-quality-explorer/index.php
+++ air-quality-explorer/index.php
@@ -1,17 +1,48 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/inc/functions.inc.php';
-require_once __DIR__ . '/inc/api.inc.php';
+
+// Start a new Guzzle Client
+$client = startNewGuzzleClient();
+
+$limit = 1000;
+
+$countries = null;
+
+// Request countries from OpenAQ
+$response_countries = generateGetRequest($client, "/v3/countries?limit={$limit}");
+// Convert JSON data to a PHP associative array
+$responseArrayCountries = generateResponseBody($response_countries);
+// Store the results
+$countries = $responseArrayCountries['results'] ?? [];
+
+// Provide an array of specific countries to display on the homepage
+$SelectedCountries = [
+    'Germany',
+    'South Africa',
+    'United Kingdom',
+    'Japan',
+    'Canada',
+    'New Zealand',
+    'Costa Rica',
+    'Italy',
+];
 
 ?>
 
 <?php require __DIR__ . '/views/header.inc.php'; ?>
 
-    <ul>
-        <?php foreach (($filteredCountriesResponseArray ?? '') as $country) { ?>
+    <ul style="list-style-type: none; margin: 0; padding: 0;">
+        <?php foreach (($countries) as $country) { ?>
+            <?php if (!in_array($country['name'], $SelectedCountries)) {
+                continue;
+            } ?>
             <li>
                 <a href="country.php?<?php echo http_build_query([
                     'country' => $country['name'],
-                    'countryId' => $country['id'],
+                    'code' => $country['code'],
+                    'id' => $country['id'],
                 ]); ?>">
                     <?php echo e($country['name']) ?>
                     (<?php echo e($country['code']); ?>)

6f986e219cdebf277597f92c2a63f7bbe8cf7e8b
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-19 13:09:36 +0200

    Request sensors from OpenAQ API based on location

--- air-quality-explorer/location_data.php
+++ air-quality-explorer/location_data.php
@@ -1,5 +1,39 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/inc/functions.inc.php';
+
+$location_id = null;
+$location_name = null;
+if (!empty($_GET['location_id'])) {
+    $location_id = $_GET['location_id'];
+    $location_name = $_GET['location_name'];
+}
+
+$client = startNewGuzzleClient();
+
+$limit = 1000;
+$currentDate = date('Y-m-d');
+
+$response_sensors = generateGetRequest($client, "/v3/locations/{$location_id}/sensors?limit={$limit}");
+$responseArraySensors = generateResponseBody($response_sensors);
+$sensors = $responseArraySensors['results'] ?? [];
+echo '<pre>';
+print_r($sensors);
+echo '</pre>';
+
+$parameters = ['pm25', 'pm10'];
+
+$measurements = [];
+
+foreach ($sensors as $sensor) {
+    if (!in_array($sensor['parameter']['name'], $parameters)) {
+        continue;
+    }
+    if (!isset($measurements)) {
+
+    }
+}
 
 ?>
 
@@ -16,6 +50,19 @@
 <body>
 
 <?php require_once __DIR__ . '/views/header.inc.php'; ?>
+
+<?php /*if (!empty($sensors) && is_array($sensors)) { */?><!--
+    <h2>Air Quality Measurements for <?php /*echo e($location_name); */?></h2>
+    <table>
+        <?php /*foreach ($sensors as $sensor) { */?>
+            <tr>
+                <td><?php /*echo $sensor['name']; */?></td>
+            </tr>
+        <?php /*} */?>
+    </table>
+<?php /*} else { */?>
+    <h2>No measurements found</h2>
+--><?php /*} */?>
 
 
 <?php require_once __DIR__ . '/views/footer.inc.php'; ?>

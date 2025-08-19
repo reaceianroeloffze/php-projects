261e844f15a2b53680d25060e751435021b5de1d
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-19 16:44:45 +0200

    Display a table with sensor results

--- air-quality-explorer/location_data.php
+++ air-quality-explorer/location_data.php
@@ -13,7 +13,6 @@ if (!empty($_GET['location_id'])) {
 $client = startNewGuzzleClient();
 
 $limit = 1000;
-$currentDate = date('Y-m-d');
 
 $response_sensors = generateGetRequest($client, "/v3/locations/{$location_id}/sensors?limit={$limit}");
 $responseArraySensors = generateResponseBody($response_sensors);
@@ -30,8 +29,21 @@ foreach ($sensors as $sensor) {
     if (!in_array($sensor['parameter']['name'], $parameters)) {
         continue;
     }
-    if (!isset($measurements)) {
 
+    $parameter = $sensor['parameter']['name'];
+    $latestMeasurement = $sensor['latest']['value'] ?? null;
+    $parameterUnits = $sensor['parameter']['units'] ?? null;
+    $parameterDisplayName = $sensor['parameter']['displayName'] ?? null;
+
+    if (!isset($measurements[$parameter])) {
+        $measurements[$parameter] = [];
+        $measurements[$parameter]['displayName'] = $parameterDisplayName;
+        $measurements[$parameter]['measurementUnits'] = $parameterUnits;
+        $measurements[$parameter]['latestMeasurementValue'] = $latestMeasurement;
+    }
+
+    if (count(array_intersect(array_keys($measurements), $parameters)) === count($parameters)) {
+        break;
     }
 }
 
@@ -45,24 +57,35 @@ foreach ($sensors as $sensor) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles/simple.css">
-    <title></title>
+    <title><?php echo $location_name ?> | Air Quality Explorer</title>
 </head>
 <body>
 
 <?php require_once __DIR__ . '/views/header.inc.php'; ?>
 
-<?php /*if (!empty($sensors) && is_array($sensors)) { */?><!--
-    <h2>Air Quality Measurements for <?php /*echo e($location_name); */?></h2>
-    <table>
-        <?php /*foreach ($sensors as $sensor) { */?>
-            <tr>
-                <td><?php /*echo $sensor['name']; */?></td>
-            </tr>
-        <?php /*} */?>
+<?php if (!empty($measurements)) { ?>
+    <h2>Measurements for <?php echo e($location_name); ?></h2>
+    <table style="width: 100%">
+        <thead>
+        <tr>
+            <th>Particle Concentration</th>
+            <th>Latest Measurement Value</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($measurements as $parameter => $measurement) { ?>
+            <?php if (!empty($measurement['latestMeasurementValue'])) { ?>
+                <tr>
+                    <td><?php echo e($measurement['displayName']); ?></td>
+                    <td><?php echo e(round($measurement['latestMeasurementValue'], 2)) . ' ' . $measurement['measurementUnits']; ?></td>
+                </tr>
+            <?php } ?>
+        <?php } ?>
+        </tbody>
     </table>
-<?php /*} else { */?>
-    <h2>No measurements found</h2>
---><?php /*} */?>
+<?php } else { ?>
+    <h2>No new measurements found for <?php echo e($location_name); ?></h2>
+<?php } ?>
 
 
 <?php require_once __DIR__ . '/views/footer.inc.php'; ?>

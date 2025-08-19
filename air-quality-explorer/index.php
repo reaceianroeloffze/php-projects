bbad3aefdaac31abefd908b399fa4fa0460e26a8
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-19 11:05:51 +0200

    Add heading and prompt paragraph

--- air-quality-explorer/index.php
+++ air-quality-explorer/index.php
@@ -32,14 +32,16 @@ $SelectedCountries = [
 ?>
 
 <?php require __DIR__ . '/views/header.inc.php'; ?>
-
+<?php if (!empty($countries) && is_array($countries)) { ?>
+    <h2>Browse Air Quality Indexes</h2>
+    <p>Select a country to view its air quality data:</p>
     <ul style="list-style-type: none; margin: 0; padding: 0;">
         <?php foreach (($countries) as $country) { ?>
             <?php if (!in_array($country['name'], $SelectedCountries)) {
                 continue;
             } ?>
             <li>
-                <a href="country.php?<?php echo http_build_query([
+                <a href="country.php?<?php echo http_build_query(data: [
                     'country' => $country['name'],
                     'code' => $country['code'],
                     'id' => $country['id'],
@@ -50,5 +52,8 @@ $SelectedCountries = [
             </li>
         <?php } ?>
     </ul>
+<?php } else { ?>
+    <h1>No data to load</h1>
+<?php } ?>
 
 <?php require __DIR__ . '/views/footer.inc.php'; ?>

f6d23924b48e38c06e39c4f99ce7e3d0fb09c1c5
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-19 11:05:05 +0200

    Ensure latest locations added to country.php

--- air-quality-explorer/country.php
+++ air-quality-explorer/country.php
@@ -28,10 +28,7 @@ print_r($locations);
 echo '</pre>';
 */
 
-$parameters = [
-    'pm25',
-    'pm10',
-];
+$currentDate = trim(date('Y-m-d'));
 
 ?>
 
@@ -42,33 +39,35 @@ $parameters = [
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $country; ?> | Air Quality Index Explorer</title>
+    <title><?php echo e($country); ?> | Air Quality Index Explorer</title>
 </head>
 <body>
 <?php require_once __DIR__ . '/views/header.inc.php'; ?>
 
 <section class="locations-container">
-    <?php if (empty($country)) { ?>
-        <h1>No country data to load</h1>
-    <?php } else { ?>
-        <h1>Air Quality Monitoring Stations in <?php echo $country; ?></h1>
-    <?php } ?>
-    <div class="locations" style="display: grid; grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); grid-gap: 1rem; align-items: center;">
-        <?php if (!empty($country)) { ?>
+    <?php if (!empty($locations) && is_array($locations)) { ?>
+        <h2>Air Quality Monitoring Stations in <?php echo e($country); ?></h2>
+        <div class="locations"
+             style="display: grid; grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); grid-gap: 1rem; align-items: center;">
             <?php foreach ($locations as $location) { ?>
-
+                <?php if (!str_contains($location['datetimeLast']['local'] ?? '', $currentDate)) {
+                    continue;
+                } ?>
                 <p class="location-name" style="margin: 0.5rem 0;">
                     <a href="location_data.php?<?php echo http_build_query([
-                            'location_id' => $location['id'],
-                        ]
-                    ) ?>">
-                        <?php echo (in_array($location['locality'], $locations)) ? $location['name'] . ', ' . $location['locality'] : $location['name'] ?>
+                                    'location_id' => $location['id'],
+                                    'location_name' => $location['name'],
+                            ]
+                    ) ?>" target="_blank" rel="noopener noreferrer">
+                        <?php echo (!empty($location['locality'])) ? e($location['name']) . ', ' . e($location['locality']) : e($location['name']) ?>
                     </a>
                 </p>
-
             <?php } ?>
-        <?php } ?>
-    </div>
+        </div>
+    <?php } else { ?>
+        <h2>No data to load</h2>
+    <?php } ?>
+
 </section>
 
 <?php require_once __DIR__ . '/views/footer.inc.php'; ?>

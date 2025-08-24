fd6ebbc1a91ec9633f521a651794810eb1414ae8
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-08-24 18:23:54 +0200

    Add favicon, dynamic title

--- air-quality-explorer/views/header.inc.php
+++ air-quality-explorer/views/header.inc.php
@@ -2,9 +2,25 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <meta name="author" content="Reace Ian Roeloffze"/>
+        <meta name="description" content="Air Quality Index Explorer">
         <link rel="stylesheet" type="text/css" href="./styles/simple.css"/>
-        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="./styles/custom.css"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io/favicon-16x16.png">
+        <link rel="manifest" href="./favicon_io/site.webmanifest">
+        <!-- Display the country name -->
+        <?php if (isset($country)) : ?>
+            <title><?php echo e($country); ?> | Air Quality Index Explorer</title>
+        <?php elseif (isset($location_name)) : ?>
+            <!-- Display the location name in the title if it exists -->
+            <title><?php echo e($location_name); ?> | Air Quality Index Explorer</title>
+        <?php else : ?>
+            <title>Home | Air Quality Index Explorer</title>
+        <?php endif; ?>
     </head>
     <body>
         <header>

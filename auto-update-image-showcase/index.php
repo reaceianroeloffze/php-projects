9d7e8a11b67f5539b0112aacbb9cfaee03ef16c7
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-09-04 10:01:02 +0200

    Add auto-update-image-showcase to repo

---
+++ auto-update-image-showcase/index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Reace Ian Roeloffze">
+        <meta name="description" content="Auto-update image showcase">
+        <link rel="stylesheet" type="text/css" href="./styles/simple.css">
+        <title>Image Showcase</title>
+    </head>
+    <body>
+        <header>
+            <h1>Image Showcase</h1>
+        </header>
+        <main>
+            <!-- Display images and their corresponding descriptions -->
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>

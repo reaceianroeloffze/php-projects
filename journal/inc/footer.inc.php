9bbd11df12d9e60e135ac060b444331c29052587
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-10-29 13:17:05 +0200

    Add footer.inc.php

---
+++ journal/inc/footer.inc.php
@@ -0,0 +1,16 @@
+</main>
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container site-footer__container">
+        <h3 class="site-footer__heading">PHP Diary</h3>
+        <p class="site-footer__description">
+            This is a PHP diary app one can use to keep track of their learning journey through PHP.
+        </p>
+        <p class="site-footer__last-updated">
+            Last updated:
+            <span class="last-updated__year"></span>
+        </p>
+    </div>
+</footer>
+</body>
+</html>

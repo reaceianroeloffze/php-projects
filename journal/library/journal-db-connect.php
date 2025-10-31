02ec91357bce76b60cb793ce147fda522d680561
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-10-31 08:13:48 +0200

    Refine PHPDoc

--- journal/library/journal-db-connect.php
+++ journal/library/journal-db-connect.php
@@ -1,12 +1,27 @@
 <?php
 
-    /* Connect to the database */
-    try {
-        $pdo = new PDO('mysql:host=localhost;dbname=journal', 'root', 'M@r!ADBK1ngR3ace56', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ]);
-        /* Catches any errors */
-    } catch (PDOException $e) {
-        echo 'Could not connect to the database.';
-        exit;
+    /** Connect to the database
+     *
+     * @return PDO <p>
+     * PHP Data Object representing the connection to the database
+     * </p>
+     * @throws PDOException <p>
+     * Catches any errors connecting to the database
+     * </p>
+     * @var PDO $pdo <p>
+     * A new PDO connection
+     * </p>
+     */
+    function connectToDatabase(): PDO
+    {
+        try {
+            $pdo = new PDO('mysql:host=localhost;port=3308;dbname=journal;charset=utf8mb4', 'root', 'M@r!ADBK1ngR3ace56', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]);
+            /* Catches any errors */
+        } catch (PDOException) {
+            echo 'Could not connect to the database.';
+            exit;
+        }
+        return $pdo;
     }

c90ad40715cbcbece4d8c283f307e2b7da0ee220
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-10-31 12:48:22 +0200

    Add CountAllJournalEntries() to model, refine docblocks

--- journal/model/journal-model.php
+++ journal/model/journal-model.php
@@ -1,7 +1,7 @@
 <?php
 
     /**
-     * Display a set amount of journal entries per page.
+     * Display a set amount of journal entries per webpage.
      *
      * @param int $entriesPerPage <p>
      * A number of <p>entries to display per page
@@ -18,27 +18,34 @@
      * @var string $sql <p>
      * Stored SQL query
      * </p>
+     * @param int $offset <p>
+     * The offset for entries to display based on the page number
+     * </p>
      * @return array <p>
      * The associative array of journal entries stored in $entires
      * </p>
      * */
-    function displayJournalEntries(int $entriesPerPage): array
+    function displayJournalEntries(int $entriesPerPage, int $offset): array
     {
         $db = connectToDatabase();
         $sql = <<<'SQL'
-            SELECT 
-                title, 
-                body, 
-                created_on 
+            SELECT
+                title,
+                body,
+                created_on
             FROM 
-                entries 
+                entries
             ORDER BY 
-                created_on DESC 
-            LIMIT 
-                :entriesPerPage;
+                created_on DESC,
+                id DESC
+            LIMIT
+                :entriesPerPage 
+            OFFSET
+                :offset;
             SQL;
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':entriesPerPage', $entriesPerPage, PDO::PARAM_INT);
+        $stmt->bindValue(':entriesPerPage', $entriesPerPage, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
         $stmt->execute();
         $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $stmt->closeCursor();
@@ -48,14 +55,30 @@
     /**
      * Insert a new journal entry into the database.
      *
-     * @param string $title <p>The title of the journal entry</p>
-     * @param string $created_on <p>The date the journal entry was created</p>
-     * @param string $body <p>The body content of the journal entry</p>
-     * @var PDO $db <p>Database connection</p>
-     * @var PDOStatement $stmt <p>Prepared statement</p>
-     * @var string $sql <p>SQL query</p>
-     * @var int $newRowCreated <p>New row added to the database</p>
-     * @return bool <p>1 if the insertion was successful, 0 otherwise</p>
+     * @param string $title <p>
+     * The title of the journal entry
+     * </p>
+     * @param string $created_on <p>
+     * The date the journal entry was created
+     * </p>
+     * @param string $body <p>
+     * The body content of the journal entry
+     * </p>
+     * @var PDO $db <p>
+     * Database connection
+     * </p>
+     * @var PDOStatement $stmt <p>
+     * Prepared statement
+     * </p>
+     * @var string $sql <p>
+     * SQL query
+     * </p>
+     * @var int $newRowCreated <p>
+     * New row added to the database
+     * </p>
+     * @return bool <p>
+     * 1 if the insertion was successful, 0 otherwise
+     * </p>
      * */
     function insertNewJournalEntry(string $title, string $created_on, string $body): bool
     {
@@ -67,11 +90,34 @@
                 (:title, :body, :created_on);
             SQL;
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-        $stmt->bindParam(':body', $body, PDO::PARAM_STR);
-        $stmt->bindParam(':created_on', $created_on, PDO::PARAM_STR);
+        $stmt->bindValue(':title', $title, PDO::PARAM_STR);
+        $stmt->bindValue(':body', $body, PDO::PARAM_STR);
+        $stmt->bindValue(':created_on', $created_on, PDO::PARAM_STR);
         $stmt->execute();
         $newRowCreated = $stmt->rowCount();
         $stmt->closeCursor();
         return $newRowCreated;
+    }
+
+    /**
+     * Count all entries in the journal database.
+     * 
+     * @return int <p>
+     * The total number of journal entries in the database
+     * </p>
+     * */
+    function countAllJournalEntries(): int
+    {
+        $db = connectToDatabase();
+        $sql = <<<'SQL'
+            SELECT 
+                COUNT(*) AS entryCount 
+            FROM 
+                entries;
+            SQL;
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $entryCount = (int) $stmt->fetchColumn();
+        $stmt->closeCursor();
+        return $entryCount;
     }
